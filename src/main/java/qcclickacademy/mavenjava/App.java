e0a4763d3b102189decfb0e11a9c47fb5ef7a0af
pornima <pornimamuchate7087@gmail.com>
2020-08-02 17:51:53 +0530

    first commit

---
+++ src/main/java/qcclickacademy/mavenjava/App.java
@@ -0,0 +1,15 @@
+package qcclickacademy.mavenjava;
+
+import io.appium.java_client.Setting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( Setting[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
